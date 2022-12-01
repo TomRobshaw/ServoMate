b1a1646a416b05f57660f39e607d84841647a068
Tom Robshaw <t.robshaw@outlook.com>
2022-12-01 14:13:01 +1100

    map added to garage index page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
@@ -8,7 +9,12 @@ export default class extends Controller {
   }
 
   connect() {
-    // [...]
+    mapboxgl.accessToken = this.apiKeyValue
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
 
