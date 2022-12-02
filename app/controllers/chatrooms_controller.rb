403acf9cc85c59a32ffac594552390b611179ddd
craig simpson <craigsimpson113@gmail.com>
2022-12-02 15:44:10 +1100

    linked chat with garage

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
     @message = Message.new
-    @chatroom = params[:id]
+    @chatroom = Chatroom.find params[:id]
+    garage_id = @chatroom.name.split("_")[1]
+    @garage = Garage.find(garage_id)
   end
-
-
 end
