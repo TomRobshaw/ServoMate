518e7b2d5047080052d22ff081f1ec6feb71b9f0
craig simpson <craigsimpson113@gmail.com>
2022-12-08 17:54:49 +1100

    no google map, instead added routes to mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -19,13 +19,34 @@ export default class extends Controller {
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
 
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+    this.map.addControl(new MapboxGeocoder ({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
+      this.map.addControl(
+        new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        }),
+        'bottom-left'
+        );
+        this.map.addControl(
+          new mapboxgl.GeolocateControl({
+          positionOptions: {
+          enableHighAccuracy: true
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true
+          })
+          );
   }
+
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+        }).setHTML(marker.info_window)
       const customMarker = document.createElement("div")
       // customMarker.style.backgroundImage = 'url("https://cdn-icons-png.flaticon.com/512/5717/5717505.png")';
       // customMarker.style.backgroundImage = 'url("https://cdn-icons-png.flaticon.com/512/3448/3448327.png")';
@@ -40,9 +61,33 @@ export default class extends Controller {
       customMarker.className = "marker"
       customMarker.style.backgroundSize = "contain"
       new mapboxgl.Marker(customMarker)
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(this.map)
+      this.map.on('mouseenter', 'places', (e) => {
+        // Change the cursor style as a UI indicator.
+        this.map.getCanvas().style.cursor = 'pointer';
+
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates).setHTML(description).addTo(map);
+        });
+
+        this.map.on('mouseleave', 'places', () => {
+        this.map.getCanvas().style.cursor = '';
+        popup.remove();
+        });
     })
   }
 
@@ -51,4 +96,5 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
 }
