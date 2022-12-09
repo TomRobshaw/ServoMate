67d1cfdb84c4b97139161ac312f1e324f76ae279
Tom Robshaw <t.robshaw@outlook.com>
2022-12-09 11:23:28 +1100

    added user image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_223020) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_233324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,6 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_223020) do
     t.string "last_name"
     t.string "address"
     t.string "nickname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
