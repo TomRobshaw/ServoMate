6941d943a55b4bdc3757986e2360dd04c0ff30fc
Tom Robshaw <t.robshaw@outlook.com>
2022-12-09 15:02:23 +1100

    finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_233324) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_034800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_233324) do
     t.string "image"
     t.bigint "car_id"
     t.date "service_date"
+    t.string "title"
     t.index ["car_id"], name: "index_service_histories_on_car_id"
   end
 
